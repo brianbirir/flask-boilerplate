7d8e2c15b7dd0f2009989f18fcdc917cdcedbf02
Brian Birir <brianbirir@gmail.com>
2019-03-26 17:07:45 +0300

    Refactor: Implement the app as a factory
    This means we can have several instances of the app when deploying or testing depending on the configurations

--- src/__init__.py
+++ src/__init__.py
@@ -2,20 +2,24 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-# load dot env
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
-dot_env_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(dot_env_path)
 
-# database configs
-database_uri = os.getenv('DATABASE_URI')
+def create_app(config_object=''):
 
-# initialize Flask app
-app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
+    # load dot env
+    app_root = os.path.join(os.path.dirname(__file__), '..')
+    dot_env_path = os.path.join(app_root, '.env')
+    load_dotenv(dot_env_path)
 
+    # database configs
+    database_uri = os.getenv('DATABASE_URI')
 
-# home page route
-@app.route('/')
-def home():
-    return 'Welcome home!'
+    # initialize Flask app
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    # home page route
+    @app.route('/')
+    def home():
+        return 'Welcome home!'
+
+    return app
