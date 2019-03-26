682800764bdaad4460c87a87341d520f5bc4cb3d
Brian Birir <brianbirir@gmail.com>
2019-03-26 16:28:28 +0300

    Add clear variable name and proper spacing between variables & comments

--- src/__init__.py
+++ src/__init__.py
@@ -2,19 +2,20 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-
 # load dot env
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
-dotenv_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(dotenv_path)
+dot_env_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dot_env_path)
 
 # database configs
 database_uri = os.getenv('DATABASE_URI')
 
+# initialize Flask app
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
 
+# home page route
 @app.route('/')
 def home():
     return 'Welcome home!'
