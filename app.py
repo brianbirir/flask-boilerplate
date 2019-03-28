9ce7c64dad10c5e0fcee9f28d693b7a1082936c6
brianbirir <brianbirir@gmail.com>
2019-03-28 20:53:17 +0300

    Implement grammar correction to comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src import create_app
 
 if __name__ == "__main__":
     """
-    This module should only bes used for development when running this application using the Flask webs server.
-    Running this is in production is not recommend hence look at other production level WSGI like Gunicorn
+    This module should only be used for development when running this application using the Flask web server.
+    Running this is in production is not recommended hence look at other production level WSGI like Gunicorn
     """
     create_app('config.DevelopmentConfig').run('0.0.0.0')
