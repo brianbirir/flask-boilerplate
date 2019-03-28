385bb2670fece4638adb6ff8de2d08ac40e30e93
brianbirir <brianbirir@gmail.com>
2019-03-28 20:51:51 +0300

    Add comments to direct developers when using dev entry module

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ from src import create_app
 
 
 if __name__ == "__main__":
+    """
+    This module should only bes used for development when running this application using the Flask webs server.
+    Running this is in production is not recommend hence look at other production level WSGI like Gunicorn
+    """
     create_app('config.DevelopmentConfig').run('0.0.0.0')
