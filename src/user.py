fe81432531764d744773b57726c3a280f459a287
brianbirir <brianbirir@gmail.com>
2019-03-29 11:46:40 +0300

    Add resource for PUT action & refactor argument parsing functions

--- src/user.py
+++ src/user.py
@@ -1,14 +1,45 @@
+from datetime import datetime
 from flask_restful import Resource, reqparse
 from src.model import UserModel
-
-parser = reqparse.RequestParser()
 
 
 class User(Resource):
 
-    def get(self):
+    @staticmethod
+    def get_user_details_parsed_args():
+        """
+        method for parsing arguments received from the request
+        :return: parsed arguments
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str,
+                            help='Email address used to login. This should be a string',
+                            required=True)
+        parser.add_argument('name', type=str, help='The name of the user', required=True)
+        return parser.parse_args()
+
+    @staticmethod
+    def get_user_id_parsed_args():
+        """
+        method for parsing user id arg received from the request
+        :return: parsed user id
+        """
+        parser = reqparse.RequestParser()
         parser.add_argument('user_id', type=int, help='The ID for the user', required=True)
-        data = parser.parse_args()
+        return parser.parse_args()
+
+    @staticmethod
+    def get_user_password_parsed_args():
+        """
+        method for parsing user password arg received from the request
+        :return: parsed user id
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('password', type=str, help='The password for the user', required=True)
+        return parser.parse_args()
+
+    def get(self):
+        data = self.get_user_id_parsed_args()
 
         try:
             user = UserModel.query.filter_by(id=data['user_id']).first()
@@ -26,31 +57,47 @@ class User(Resource):
             return {"message": str(e)}, 500
 
     def post(self):
-        parser.add_argument('email', type=int, help='Email address used to login', required=True)
-        parser.add_argument('name', type=str, help='The name of the user', required=True)
-        parser.add_argument('password', type=str, help='The password for the user', required=True)
-        data = parser.parse_args()
+        data_user_details = self.get_user_details_parsed_args()
+        data_password = self.get_user_password_parsed_args()
+
         user = UserModel(
-            email=data['email'],
-            name=data['name'],
-            password=UserModel.generate_hash(data['password'])
+            email=data_user_details['email'],
+            name=data_user_details['name'],
+            password=UserModel.generate_hash(data_password['password'])
         )
 
         try:
             user.save_to_db()
-            return {"message": "User {} was created".format(data['name'])}, 200
+            return {"message": "User {} was created".format(data_user_details['name'])}, 200
         except Exception as e:
             return {"message": str(e)}, 500
 
-    def update(self):
-        pass
-
-    def delete(self):
-        parser.add_argument('user_id', type=int, help='The ID for the user', required=True)
-        data = parser.parse_args()
+    def put(self):
+        data_user_id = self.get_user_id_parsed_args()
+        data_user_details = self.get_user_details_parsed_args()
+        data_password = self.get_user_password_parsed_args()
 
         try:
-            user = UserModel.query.filter_by(id=data['user_id']).first()
+            user = UserModel.query.filter_by(id=data_user_id['user_id']).first()
+
+            if user:
+                user.email = data_user_details['email']
+                user.name = data_user_details['name']
+                user.password = UserModel.generate_hash(data_password['password'])
+                user.updated_at = datetime.utcnow()
+                user.save_to_db()
+                return {"message": "User {} was updated".format(data_user_details['name'])}, 200
+            else:
+                return {"message": "This user does not exist"}, 404
+
+        except Exception as e:
+            return {"message": str(e)}, 500
+
+    def delete(self):
+        data_user_id = self.get_user_id_parsed_args()
+
+        try:
+            user = UserModel.query.filter_by(id=data_user_id['user_id']).first()
 
             if user:
                 user.delete_from_db()
