f3e2e65ed26a52e38b49b0e8b8ae15e5550dc9ec
brianbirir <brianbirir@gmail.com>
2019-04-07 12:21:24 +0300

    Refactor: Store current time in a variable and add JWT decode exceptions
    - access current time as a variable to be able to compute accurate expiry time

--- src/utils/security/jwt_security.py
+++ src/utils/security/jwt_security.py
@@ -9,12 +9,13 @@ def set_payload(subject_id):
     :param subject_id:
     :return: dictionary object
     """
-    # set expiry time for token to be 24 hours from time of creation
-    expiry_time = datetime.utcnow() + timedelta(hours=24)
+    # set expiry time for token to be 2 hours from time of creation
+    current_time = datetime.utcnow()
+    expiry_time = current_time + timedelta(hours=24)  # add 2 hours to the current time
 
     payload = {
         "sub": subject_id,
-        "iat": datetime.utcnow().timestamp(),
+        "iat": current_time.timestamp(),
         "exp": expiry_time.timestamp()
     }
     return payload
@@ -27,19 +28,19 @@ def encode_jwt(subject_id='', secret='', alg='HS256'):
     """
     try:
         return jwt.encode(set_payload(subject_id), secret, algorithm=alg)
-    except jwt.ExpiredSignatureError:
-        return "Token signature has expired. Please log in!"
     except Exception as e:
         return str(e)
 
 
 def decode_jwt(encoded_jwt, secret='', alg='HS256'):
     """
-    Generate JWT authentication token
+    Decode JWT authentication token
     :return: int
     """
     try:
         payload = jwt.decode(encoded_jwt, secret, algorithms=alg)
         return payload['sub']
-    except Exception as e:
-        return str(e)
+    except jwt.ExpiredSignatureError:
+        return "Signature expired. Please log in again."
+    except jwt.InvalidTokenError:
+        return "Invalid token. Please log in again."
